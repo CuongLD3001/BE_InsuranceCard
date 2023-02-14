aa4d317e40976b63c685e8da8c6aeccf8be459b3
Annthe160639 <annthe160639@fpt.edu.vn>
2023-02-14 22:57:53 +0700

    first commit

---
+++ src/test/java/com/swp/g3/InsuranceCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.g3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
