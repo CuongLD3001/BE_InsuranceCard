e5cb6c59a79bd456437d72ee650e45a4c71d410d
Annthe160639 <annthe160639@fpt.edu.vn>
2023-02-16 23:29:51 +0700

    update

--- src/main/java/com/swp/g3/InsuranceCardApplication.java
+++ src/main/java/com/swp/g3/InsuranceCardApplication.java
@@ -16,21 +16,4 @@ public class InsuranceCardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InsuranceCardApplication.class, args);
 	}
-
-	@Bean(name = "dataSource")
-	public DriverManagerDataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		dataSource.setUrl("jdbc:sqlserver://localhost:1433;databaseName=demo_spring_boot;encrypt=true;trustServerCertificate=true;");
-		dataSource.setUsername("g3");
-		dataSource.setPassword("123456");
-
-		// schema init
-		Resource initSchema = new ClassPathResource("schema.sql");
-		Resource initData = new ClassPathResource("data.sql");
-		DatabasePopulator databasePopulator = new ResourceDatabasePopulator(initSchema, initData);
-		DatabasePopulatorUtils.execute(databasePopulator, dataSource);
-
-		return dataSource;
-	}
 }
